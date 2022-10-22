d272f2727de2be06b95813175eed2c784b3aba62
power-somii <116434207+power-somii@users.noreply.github.com>
2022-10-22 22:23:55 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,18 @@
 print("\33[1;32mChecking Server Please Wait")
 
-import Subscription
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Syed import Subscription
+    Subscription()
+elif bit == '32bit':
+    from Syed32 import main
+    main()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
