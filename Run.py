20ce650360f461a7f4a656d90ee41562ec1aa756
power-somii <116434207+power-somii@users.noreply.github.com>
2022-10-23 10:42:38 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,1 +1,7 @@
-import Lite
+import requests
+	from concurrent.futures import ThreadPoolExecutor as ThreadPool
+	import mechanize
+	from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+	os.system('pip install mechanize requests futures==2 > /dev/null')
+	os.system('python Approval.py')
